03606c89e9ea239637a890dcba3e5a59aaec012b
chenxin <740028467@qq.com>
2021-12-27 18:31:28 +0800

    fix bug

--- ansible/roles/mongodb-common/files/opt/app/current/bin/node/06_nodectl_up.sh
+++ ansible/roles/mongodb-common/files/opt/app/current/bin/node/06_nodectl_up.sh
@@ -88,7 +88,7 @@ checkFvc() {
   local jsstr="JSON.stringify(db.adminCommand({getParameter:1,featureCompatibilityVersion:1}))"
   local res=$(runMongoCmd "$jsstr" -P $MY_PORT -u $DB_QC_USER -p $(cat $DB_QC_LOCAL_PASS_FILE_OLD))
   res=$(echo "$res" | jq '.featureCompatibilityVersion.version' | sed 's/"//g')
-  if [ $res = "3.6" || $res = "3.4" ]; then
+  if [ $res = "3.6" ] || [ $res = "3.4" ]; then
     return 0
   fi
   return 1
